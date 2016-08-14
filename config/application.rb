5e458b2d30c56e65d1766ff0961430805ea62194
Henry Arbolaez <henryarbolaez@gmail.com>
2016-08-14 03:05:33 -0400

    Update cors

--- config/application.rb
+++ config/application.rb
@@ -18,13 +18,11 @@ Bundler.require(*Rails.groups)
 
 module Medmotionapi
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://guarded-atoll-78576.herokuapp.com/patients',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
     config.api_only = true
   end
 end
